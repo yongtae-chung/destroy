d82545cd11c2d0c98bdfc28916f8adc870785d81
yongtae-chung <jyt5009@gmail.com>
2018-09-29 22:04:16 +0900

    1차 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './List'
 
+
+const target = [
+  {
+    name: "존맛탱a",
+    description: "전나 맛있는 집임",
+
+  }
+]
 class App extends Component {
   render() {
     return (
@@ -11,8 +20,9 @@ class App extends Component {
           <h1 className="App-title">개박살 리스트</h1>
         </header>
         <p className="App-intro">
-          수도권 일대의 맛집을 개박살 내기 위한 리스트.
+          수도권 일대의 맛집을 개박살 내기 위한 리스트. 존맛탱 리스트
         </p>
+        <List title={target.name}/>
       </div>
     );
   }
---
+++ src/List.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './List.css';
+
+class List extends Component {
+  render() {
+    return (
+      <table className="listTalbe"></table>
+    );
+  }
+}
+
+export default List;
